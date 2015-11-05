d48296923040b84f247d2b9eb983d9bacf5f7900
John Richardson <richardsonjm@gmail.com>
2015-11-05 15:28:47 -0500

    Add bunlder

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'json'
+require 'rgl/adjacency'
+require 'rgl/dot'
+
+Bundler.require
--- src/tweets_cleaned.rb
+++ src/tweets_cleaned.rb
@@ -1,4 +1,4 @@
-require 'json'
+require_relative '../config/environment.rb'
 
 tweets_raw = File.read('./tweet_input/tweets.txt')
 tweets_clean = File.open('./tweet_output/ft1.txt', 'w')
@@ -17,8 +17,10 @@ tweets_clean.write("\n")
 tweets_clean.write("#{unicode_counter.to_s} tweets contained unicode.")
 tweets_clean.close
 
-matrix = []
 File.read(tweets_clean).each_line do |line|
   hashtags = line.scan(/#([A-Za-z0-9]+)/).flatten
   p hashtags if hashtags.any?
+  ug = RGL::DirectedAdjacencyGraph[hashtags.join(', ')]
+  # ug = RGL::DirectedAdjacencyGraph[1,2 ,2,3 ,2,4, 4,5, 6,4, 1,6]
+  p ug
 end
