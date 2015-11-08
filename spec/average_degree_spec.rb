80bcbefd41ed06aee9de2fce4ccd705c444d54c0
John Richardson <richardsonjm@gmail.com>
2015-11-08 16:33:42 -0500

    test proving code is broken

---
+++ spec/average_degree_spec.rb
@@ -0,0 +1,27 @@
+require_relative './spec_helper.rb'
+
+describe AverageDegree do
+  before do
+    allow(File).to receive(:read)
+    allow(File).to receive(:open)
+    @average_degree = AverageDegree.new('source', 'destination', 60)
+  end
+
+  subject { @average_degree }
+
+  context "#avg_degree" do
+    it "doesn't double count existing nodes" do
+      foo = 'foo'
+      allow(File).to receive(:open)
+      allow(File).to receive(:write)
+      allow(foo).to receive(:write)
+      timestamp = DateTime.strptime("Thu Oct 29 17:51:50 +0000 2015", "%a %b %d %k:%M:%S %z %Y")
+      @average_degree.instance_variable_set(:@timestamp, timestamp)
+      @average_degree.instance_variable_set(:@hashtag_sets, [['foo', 'bar']])
+      @average_degree.instance_variable_set(:@hashtags, ['foo', 'bar'])
+      @average_degree.instance_variable_set(:@destination, foo)
+      @average_degree.avg_degree(timestamp, ['foo', 'bar'])
+      expect(@average_degree.instance_variable_get(:@hashtags)).to eq ['foo', 'bar']
+    end
+  end
+end
